821daef52d7e2b5a2095ca8dfb621fb83a7f9a22
Dimitris Simeonidis <symeon.dev@gmail.com>
2020-06-01 19:49:44 +0300

    Template files added

---
+++ controllers/apiKey_template.js
@@ -0,0 +1,5 @@
+// This is a template file for apiKey.js
+// You have to replace the 'apiKey' value with your Clarifai API key and rename this file to 'apiKey.js'
+module.exports = {
+    apiKey: '0123456789abcdef0123456789abcdef'
+}
