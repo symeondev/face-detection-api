ab5f5e7f71f4b223b88cd0c21ef2860f6679b314
Dimitris Simeonidis <symeon.dev@gmail.com>
2020-06-03 17:31:03 +0300

    Rename API URL

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send('it is working'); });
+app.get('/', (req, res) => { res.send('Face detection server is running...'); });
 app.post('/signin', signin.handleSignin(db, bcrypt));
 app.post('/register', (req, res) => register.handleRegister(req, res, db, bcrypt));
 app.get('/profile/:id', (req, res) => profile.handleProfileGet(req, res, db));
